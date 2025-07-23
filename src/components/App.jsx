469e2600345f38920d21802dddea2724673ff6cc
Vihaan <vihaan@Vihaans-MacBook-Pro.local>
2025-07-23 13:55:23 +0400

    Updates

--- src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import MultiSectionPage from './MultiSectionPage.jsx';
-
-export default function App() {
-  return (
-    <main className="app-root">
-      <MultiSectionPage />
-    </main>
-  );
-}
