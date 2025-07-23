4ed24d402b3b445ea86cc44bd9671ed08db99c37
Vihaan <vihaan@Vihaans-MacBook-Pro.local>
2025-07-23 15:24:48 +0400

    Add minimal Astro + Preact site

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import MultiSectionPage from './MultiSectionPage.jsx';
+
+export default function App() {
+  return (
+    <main className="app-root">
+      <MultiSectionPage />
+    </main>
+  );
+}
