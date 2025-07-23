03c05488478c96c902eca1e24d895cbf056f04b2
Vihaan <vihaan@Vihaans-MacBook-Pro.local>
2025-07-23 14:59:13 +0400

    Setup clean deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import preact from '@astrojs/preact';
 export default defineConfig({
   site: 'https://aurumz-dev.github.io',
   base: '/',
-  integrations: [ preact() ],
+  integrations: [preact()],
 });
