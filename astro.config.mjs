a66ab89585f690b4596362807c00b21e8df90418
Vihaan <vihaan@Vihaans-MacBook-Pro.local>
2025-07-23 13:43:42 +0400

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 
 export default defineConfig({
-  base: "/", // this is correct for aurumz-dev.github.io
+  integrations: [preact()],
+  // ...other config options
 });
